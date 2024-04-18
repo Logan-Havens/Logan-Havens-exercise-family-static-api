bee3183aaff23e2e1aa8a73e87d1abfaa8fdb366
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-04-18 00:19:38 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -1,44 +1,56 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+def get_members():
+    return jsonify(jackson_family.get_all_members()), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member)
 
-# this only runs if `$ python src/app.py` is executed
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    data = request.get_json()
+    jackson_family.add_member(data)
+    return jsonify({"message": "Member added successfully"}), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    result = jackson_family.delete_member(id)
+    if not result:
+        return jsonify({'message': 'Member not found'}), 404
+    return jsonify({'done': True})
+
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    data = request.get_json()
+    updated_member = jackson_family.update_member(member_id, data)
+    if updated_member:
+        return jsonify({"message": "Member updated successfully"}), 200
+    return jsonify({"message": "Member not found"}), 404
+
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(debug=True)
+
